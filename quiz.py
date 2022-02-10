77ff0a41a24ccbc3eb0aa3d0445823268d1719dd
Grim4521 <james.stead12@gmail.com>
2022-02-10 19:37:27 +0000

    Done some general work on my quiz just adding more stuff to it.

--- quiz.py
+++ quiz.py
@@ -1,2 +1,17 @@
-print ("hello world")
-# this is another test
+import time
+print ("Hello and welcome to my Quiz")
+time.sleep(1)
+while True:
+    yes = input ("If you want to start please type yes ")
+    if yes.lower() == "yes":
+        print ("Lets start")
+        break
+else:
+    print ("Goodbye")
+    exit()
+# try and put a function above for both of the outcomes in the while loop
+print ("Now then Lets continue")
+time.sleep(0.5)
+
+
+
