6871c1be8c850da10beed29a850a2ad2a8d9412c
Grim4521 <james.stead12@gmail.com>
2022-02-10 20:21:23 +0000

    Added more, changed "failure" to "You are wrong".

--- quiz.py
+++ quiz.py
@@ -18,12 +18,22 @@ time.sleep(1)
 print ("round 1!")
 time.sleep(0.5)
 print ("This round is on maths")
-question1 = input ("What is 56x45")
+question1 = input ("What is 56x45 ")
 if question1 == "2520":
     print("Well done")
     f.write("1")
 else:
-    print ("failure")
+    print ("You are wrong ")
+    print ("The answer was 2,520")
+time.sleep(1)
+print ("question 2")
+answer2 = input ("What is the captial of Amercia")
+if answer2.lower() == "Washington DC":
+    print ("Well done you are correct")
+    f.write ("1")
+else:
+    print ("you are wrong")
+
 
 
 
