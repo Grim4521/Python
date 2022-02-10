a00f450a710c56e82cc94648c32818e74eb033a2
Grim4521 <james.stead12@gmail.com>
2022-02-10 19:55:42 +0000

    Just added more code onto my quiz.

--- quiz.py
+++ quiz.py
@@ -1,3 +1,4 @@
+f = open("QuizScore.txt","x")
 import time
 print ("Hello and welcome to my Quiz")
 time.sleep(1)
@@ -12,6 +13,18 @@ else:
 # try and put a function above for both of the outcomes in the while loop
 print ("Now then Lets continue")
 time.sleep(0.5)
+print ("There will be 5 questions.")
+time.sleep(1)
+print ("round 1!")
+time.sleep(0.5)
+print ("This round is on maths")
+question1 = input ("What is 56x45")
+if question1 == "2520":
+    print("Well done")
+    f.write("1")
+else:
+    print ("failure")
+
 
 
 
